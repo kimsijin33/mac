0c0b0c839190786c84f8951614af58b6a64707ef
KimSiJin <kimsijin33@gmail.com>
2024-12-08 17:04:41 +0900

    p111 까지

--- showFigCPP.cpp
+++ showFigCPP.cpp
@@ -287,5 +287,82 @@ int main()
 	PrintPersonPtr(&shain1);
 	PrintPersonVal(shain1);
 
+//p110
+#include <iostream>
+#include <string.h>
 
+using namespace std;
+
+classs Student {	//학생 클래스
+public:
+	void SetID(int num) { id = num; }
+	void SetName(char *str) {
+		strcpy(name, str);
+	}
+	int GetID( ) { return id; }
+	char *GetName ( ) const {
+	}
+private:
+	int id;	//학생 ID
+	char name[30]	//이름
+};
+
+enum Subjects { Math, English, Science };
+public:
+	void SetInfo(int id, char *name, Subjects s, int num);
+	int GetPoint() const { return point;}
+	void GetResult(char *buf) const;
+
+	Student student;
+
+pritvate:
+	Subjects subject; //과목
+	int point;
+};
+
+void Exam::SetInfo(int id, char *name, Subjects s, int num)
+{
+	student.SetID(id);
+	student.SetName(name);
+	subject = s;
+	point = num;
+}
+
+void Exam::GetResult(char *buf) const
+{
+	char *subname[] = { "수학", "영어", "과학" };
+	sprintf(buf, "%s : %d점", subnmae[subject], point );
+}
+
+void PrintResult(const Exam &Exam)
+{
+	cout << Exam.student.GetName() << endl;
+	char buf(30);
+	Exam.GetResult(buf);
+	cout << buf << endl;
+}
+
+double GetAvg(const Exam *Exam, int num) //평균값을 구한다.
+{
+	double sum = 0;
+	for(int i=0; i < num; i++) {
+		sum += Exam[i].GetPoint();
+	}
+	return sum / num;
+}
+
+int main()
+{
+	Exam Exam[3]
+	Exam[0].SetInfo( 1, "홍길동", Math, 60);
+	Exam[1].SetInfo( 1, "홍길동", English, 75);  
+	Exam[2].SetInfo( 1, "홍길동", Science, 88);
+
+	PrintResult( Exam[0] );
+	PrintResult( Exam[1] );
+	PrintResult( Exam[2] );
+	cout << endl;
+	cout << "" << GetAVg(Exam, 3) << "" << endl;
+	return 0;
+}
 
